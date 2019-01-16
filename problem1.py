698b8fcda449b84bc7f0ba0bf7ce3009ddec14a7
kailaaa <44173171+kailaaa@users.noreply.github.com>
2019-01-16 15:23:08 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,9 @@
+print('-'*65)
+print('hungry Decisoin Bot')
+print()
+answer = input('Are you hungry? ')
+if answer == 'Yes' or answer == 'yes':
+	print('Ha, too bad!!!')
+else:
+	print('Damnn, that sucks')
+print('-'*65)
---
+++ problem2.py
@@ -0,0 +1,9 @@
+print('-'*65)
+print('Best Borough Bot:')
+print()
+borough = input('What borough are you from? ')
+if borough == 'manhattan':
+	print('Nice, manhattan is the best borough!')
+else:
+	print('Too bad, manhattan is the best borough!')
+print('-'*65)
